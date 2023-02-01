4c5a9bb842de731ca6938d7fd07d68bc15ba87d5
John Paul Apolinar <john.apolinar@gmail.com>
2023-01-31 18:55:23 -0800

    Implemented Walking
    - Now goes in all 8 directions!
    - Now also plays sounds when moving up, down, left and right. These sounds play every time the frame of animation changes.
    - Currently bugged, when the player goes in a diagonal footsteps will not play.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,35 +8,56 @@ using UnityEngine.InputSystem;
 // TODO - This entire thing needs to be refactored badly. 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    // Player Controls 
+    public Rigidbody2D body;
+    public SpriteRenderer spriteRenderer;
+    public float walkSpeed;
     public InputAction playerControls;
+
+
+
+    // Animation Logic
+    public float frameRate;
+    float idleTime;
+
+    public List<Sprite> nSprites;
+    public List<Sprite> neSprites;
+    public List<Sprite> eSprites;
+    public List<Sprite> seSprites;
+    public List<Sprite> sSprites;
+
     public Vector2 moveDirection = Vector2.zero;
-    public float moveSpeed = 5f;
+
+    // SOUNDS
+
+    // Footsteps 
+    int prevFrame = -1;
 
     private enum CURRENT_TERRAIN { CARPET, TILE, WOOD };
-
-    float timer = 0.0f;
-
-    [SerializeField]
-    float footstepSpeed = 0.3f;
-
     [SerializeField]
     private CURRENT_TERRAIN currentTerrain;
-
     [SerializeField]
     private AK.Wwise.Event footstepsEvent;
-
     [SerializeField]
     private AK.Wwise.Switch[] terrainSwitch;
 
+    private void OnEnable()
+    {
+        body = GetComponent<Rigidbody2D>();
+        playerControls.Enable();
+    }
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        Debug.Log("Baby is walking on " + collider.gameObject.layer);
+        DetermineTerrain(collider);
+    }
 
+    private void DetermineTerrain(Collider2D collider)
+    {
         if (collider.gameObject.layer == LayerMask.NameToLayer("Wood"))
         {
             currentTerrain = CURRENT_TERRAIN.WOOD;
-        } 
+        }
         if (collider.gameObject.layer == LayerMask.NameToLayer("Carpet"))
         {
             currentTerrain = CURRENT_TERRAIN.CARPET;
@@ -44,14 +66,6 @@ public class PlayerController : MonoBehaviour
         {
             currentTerrain = CURRENT_TERRAIN.TILE;
         }
-
-        // Default case 
-        else
-        {
-            currentTerrain = CURRENT_TERRAIN.WOOD;
-        }
-
-        Debug.Log(currentTerrain); 
     }
 
     public void SelectAndPlayFootstep()
@@ -82,33 +96,88 @@ public class PlayerController : MonoBehaviour
         AkSoundEngine.PostEvent(footstepsEvent.Id, this.gameObject);
     }
 
-    private void OnEnable()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        playerControls.Enable();
-        
-    }
-
     private void Update()
     {
-        moveDirection = playerControls.ReadValue<Vector2>();
+        moveDirection = playerControls.ReadValue<Vector2>().normalized;
+        body.velocity = moveDirection * walkSpeed;
 
-        if (rb.velocity.magnitude > 1f && Mathf.Abs(rb.velocity.y) < 0.1f)
+        HandleSpriteFlip();
+
+        List<Sprite> directionSprites = GetSpriteDirection();
+
+        if (directionSprites != null) // holding a direction 
         {
-            if (timer > footstepSpeed)
+            
+            float playTime = Time.time - idleTime;
+            if (playTime != 0)
             {
-                SelectAndPlayFootstep();
-                timer = 0.0f;
+                int totalFrames = (int)(playTime * frameRate);
+                int frame = totalFrames % directionSprites.Count;
+                spriteRenderer.sprite = directionSprites[frame];
+
+                if (frame != prevFrame)
+                {
+                    prevFrame = frame;
+                    SelectAndPlayFootstep();
+                }
+
             }
 
-            timer += Time.deltaTime;
+        }
+        else // holding nothing 
+        {
+            idleTime = Time.time;
         }
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private List<Sprite> GetSpriteDirection()
     {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed * Time.deltaTime,
-                                  moveDirection.y * moveSpeed * Time.deltaTime);
+        List<Sprite> selectedSprites = null;
+
+        if (moveDirection.y > 0) // north
+        {
+            if (Mathf.Abs(moveDirection.x) > 0) // east or west
+            {
+                // Uncomment this if we implement 8-Way directionals
+                //selectedSprites = neSprites;
+            }
+            else
+            { // neutral x 
+                selectedSprites = nSprites;
+            }
+        }
+        else if (moveDirection.y < 0) // south 
+        {
+            if (Mathf.Abs(moveDirection.x) > 0) // east or west
+            {
+                // Uncomment this if we implement 8 - Way directionals
+                //selectedSprites = seSprites;
+            }
+            else
+            { // neutral x 
+                selectedSprites = sSprites;
+            }
+        }
+        else // neutral
+        {
+            if (Math.Abs(moveDirection.x) > 0)
+            {
+                selectedSprites = eSprites;
+            }
+        }
+
+        return selectedSprites;
+    }
+
+    private void HandleSpriteFlip()
+    {
+        if (!spriteRenderer.flipX && moveDirection.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (spriteRenderer.flipX && moveDirection.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
     }
 }
