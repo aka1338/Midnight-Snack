d7127a40deaad7734a97c11fe4de170f5eff76ed
John Paul Apolinar <john.apolinar@gmail.com>
2023-02-11 10:54:04 -0800

    Added sprites and setup

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,6 @@
+using DG.Tweening;
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,7 +9,10 @@ public class GameManager : MonoBehaviour
     // gameFirstBooted
     public bool isGameOver = false; 
     public bool playerHasSnack = false;
-    public CanvasGroup gameOverCanvas; 
+    public CanvasGroup gameLostCanvas;
+    public CanvasGroup gameWonCanvas;
+    public CanvasGroup gameStartCanvas;
+
 
     public static GameManager current;
     public GameObject snackLocation;
@@ -32,10 +38,20 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        gameOverCanvas.alpha = 0; 
+        gameLostCanvas.alpha = 0;
+        gameWonCanvas.alpha = 0;
+        gameStartCanvas.alpha = 1; 
+
         current = this;
         GameEvents.current.onSnackObtained += OnSnackObtained;
         GameEvents.current.onGameOver += SetGameOver; 
+    }
+
+
+    public void CloseOpeningScreen()
+    {
+        Debug.Log("Trying to close window here!"); 
+        gameStartCanvas.DOFade(0, 1f).SetEase(Ease.InOutQuad);
     }
 
     private void OnDisable()
@@ -52,15 +68,16 @@ public class GameManager : MonoBehaviour
     public void SetGameOver(bool state) {
 
         isGameOver = true;
-        gameOverCanvas.alpha = 1; 
 
         if (state)
         {
             Debug.Log("You won!");
+            gameWonCanvas.DOFade(1, 1f).SetEase(Ease.InOutQuad);
         }
         else
         {
             Debug.Log("You lost!");
+            gameLostCanvas.DOFade(1, 1f).SetEase(Ease.InOutQuad);
         }
         // Display a Game Over UI that allows the player to restart the game 
     }
